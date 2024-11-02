7eb0bc50450159c866cb3d046b859017fe9ca0e5
ItsKruistz <lekruistz@gmail.com>
2024-11-02 08:55:28 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -179,7 +179,7 @@
       <div class="flex items-center">
         <h1 class="text-lg font-semibold md:text-2xl">Inventory</h1>
       </div>
-      <div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+      <div class="flex flex-1 items-center justify-center rounded-xl border border-dashed">
         <div class="flex flex-col items-center gap-1 text-center">
           <h3 class="text-2xl font-bold tracking-tight">You have no products</h3>
           <p class="text-muted-foreground text-sm">
